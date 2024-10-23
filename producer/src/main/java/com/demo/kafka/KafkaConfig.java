01d6c5f49a5274b18f280346e90fc4aa7bb81fc0
unknown <23350537@telkomsel.co.id>
2024-10-23 16:15:21 +0700

    feat:kafka demo"

--- producer/src/main/java/com/demo/kafka/KafkaConfig.java
+++ producer/src/main/java/com/demo/kafka/KafkaConfig.java
@@ -20,7 +20,7 @@
 //        Map<String, Object> configProps = new HashMap<>();
 //        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 //        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-//        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 //        return new DefaultKafkaProducerFactory<>(configProps);
 //    }
 //
